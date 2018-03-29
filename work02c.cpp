89e02e80208a1548ae515276d3d95c5c09b4fc35
慕 <dj.snowman11@gmail.com>
2018-03-29 16:43:58 +0800

    第2周 作業做完

--- work02c.cpp
+++ work02c.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 #include<cstdlib>
 #include<math.h>
-#include<cmath>
 using namespace std;
 
 typedef struct{
@@ -10,36 +9,40 @@ typedef struct{
 }point;
 
 typedef struct{
-	double r;
-	double theta;
-}polar;
+	point a;
+	point b;
+}vector;
 
-vector input(void);
-vector add(vector,double,double);
-void output(vector);
+vector input(char);
+vector count1(vector,vector);
+double count2(vector);
+void output(vector,double);
 
 int main(void){
-	vector data,ans;
-	double x,y;
+	vector data1,data2,ans;
+	double reg;
 	while(1){
-		data = input();
-		if(sqrt(pow((data.a.x-data.b.x),2)+pow((data.a.y-data.b.y),2))==0){
+		data1 = input('A');
+		if(sqrt(pow((data1.a.x-data1.b.x),2)+pow((data1.a.y-data1.b.y),2))==0){
 			break;
 		}
-		printf("(x,y)=");
-		scanf("%lf %lf",&x,&y);
-		ans = add(data,x,y);
-		output(ans);
+		data2 = input('B');
+		if(sqrt(pow((data2.a.x-data2.b.x),2)+pow((data2.a.y-data2.b.y),2))==0){
+			break;
+		}
+		ans = count1(data1,data2);
+		reg = count2(ans);
+		output(ans,reg);
 	}
 	
 	system("pause");
 	return 0;
 }
 
-vector input(void){
+vector input(char reg){
 	vector data;
 	
-	printf("A->\n");
+	printf("%c->\n",reg);
 	printf("Tail(x,y)=");
 	scanf("%lf %lf",&data.a.x,&data.a.y);
 	printf("Head(x,y)=");
@@ -48,20 +51,20 @@ vector input(void){
 	return data;
 	
 }
-vector add(vector data,double x,double y){
+vector count1(vector data1,vector data2){
 	vector ans;
-	double regx,regy;
-	ans.a.x = x;
-	ans.a.y = y;
-	regx = data.b.x - data.a.x;
-	regy = data.b.y - data.a.y;
-	ans.b.x = x + regx;
-	ans.b.y = y + regy;
-	ans.ans = sqrt(pow((data.a.x-data.b.x),2)+pow((data.a.y-data.b.y),2));
+	ans.a = data1.a;
+	ans.b.x = (data2.b.x-data2.a.x)+data1.b.x;
+	ans.b.y = (data2.b.y-data2.a.y)+data1.b.y;
 	return ans;
 }
-void output(vector ans){
-	printf("New A-> (%.0lf,%.0lf) -> (%.0lf,%.0lf)\n",ans.a.x,ans.a.y,ans.b.x,ans.b.y);
-	printf("Length of vector = %lf\n",ans.ans);
+double count2(vector data){
+	double reg;
+	reg = sqrt(pow((data.a.x-data.b.x),2)+pow((data.a.y-data.b.y),2));
+	return reg;
+}
+void output(vector ans,double reg){
+	printf("(A+B)->(%.0lf,%.0lf)->(%.0lf,%.0lf)\n",ans.a.x,ans.a.y,ans.b.x,ans.b.y);
+	printf("Length of vector = %lf\n",reg);
 	return ;
 }
